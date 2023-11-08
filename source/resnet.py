5c48a10c4153fa913f49b5a97d4d77444e281cc3
Huỳnh Khắc Tâm <tamhuynhkhac@gmail.com>
2023-11-08 15:40:56 +0700

    completed resnet architecture

--- source/resnet.py
+++ source/resnet.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 
 from residual_block import Residual_Block
+from layer_maker import _make_layer
 
 class ResNet18(nn.Module):
     def __init__(self,
@@ -9,11 +10,38 @@ class ResNet18(nn.Module):
                  layers,
                  num_classes: int = 10000):
         super().__init__()
-
+        
+        self.make_layer = _make_layer
         self.inplanes = 64
 
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False)
         self.bn1 = nn.BatchNorm2d(self.inplanes)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+        self.layer1 = self.make_layer(block, self.inplanes, 64, layers[0])
+        self.layer2 = self.make_layer(block, self.inplanes, 128, layers[1], stride=2)
+        self.layer3 = self.make_layer(block, self.inplanes, 256, layers[2], stride=2)
+        self.layer4 = self.make_layer(block, self.inplanes, 512, layers[3], stride=2)
+
+        self.avgpool = nn.AdaptiveAvgPool2d((1,1))
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)           # 224x224
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)         # 112x112
+
+        x = self.layer1(x)          # 56x56
+        x = self.layer2(x)          # 28x28
+        x = self.layer3(x)          # 14x14
+        x = self.layer4(x)          # 7x7
+
+        x = self.avgpool(x)         # 1x1
+        x = torch.flatten(x, 1)     # convert 1 X 1 to vector
+        x = self.fc(x)
+
+        return x
+
 
 
